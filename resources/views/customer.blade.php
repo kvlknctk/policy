81daee278349f1d41e41f4fb2c934b2e95a647e0
kvlknctk <iletisim@volkancatak.com.tr>
2019-02-22 15:34:10 +0300

    müşteri ekleme butonu

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -8,6 +8,7 @@
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Müşteriler</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Yeni Müşteri</a>
         </div>
 
         <!-- Content Row -->
